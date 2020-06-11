39d01079954d03e2492d47aa71603db0040305fd
Tejendra Khatri <khatrite@mnstate.edu>
2020-06-10 19:07:23 -0500

    userDetails finalizedd

--- Deck_Estimator/src/sample/Main.java
+++ Deck_Estimator/src/sample/Main.java
@@ -13,16 +13,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         String startPage = "UserDetails.fxml";
+        String stageTitle = "User Details";
         Parent root;
-//        File myObj  = new File("userDetails.txt");
-//        if (myObj.createNewFile()) {
-//            System.out.println("File created: " + myObj.getName());
-//        } else {
-//            System.out.println("File already exists.");
-//            startPage = "CustomerDetails.fxml";
-//        }
+        File myObj  = new File("userDetails.txt");
+        if (myObj.createNewFile()) {
+            System.out.println("File created: " + myObj.getName());
+        } else {
+            System.out.println("File already exists.");
+            startPage = "CustomerDetails.fxml";
+            stageTitle = "Customer Details";
+        }
         root = FXMLLoader.load(getClass().getResource(startPage));
-        primaryStage.setTitle("User Details");
+        primaryStage.setTitle(stageTitle);
         primaryStage.setScene(new Scene(root, 500, 230));
         primaryStage.show();
     }
